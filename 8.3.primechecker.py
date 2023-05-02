d5b50dc7b313e6e15b2e3215ac7b4ac571c2b3af
dip980 <khanaldipendra381@gmail.com>
2023-05-02 18:00:10 +0545

    comment added

--- 8.3.primechecker.py
+++ 8.3.primechecker.py
@@ -1,8 +1,9 @@
+#prime checker
 def prime_checker(number):
 
-        if number%2==1:
+        if number%2==1: #if the remainder 1 then the number is prime 
                 print("the number is prime number\n")
-        else:
+        else:           #if the remainder 0 then the numnber isnot prime number
                 print("The number isnot a prime number")
 
 n=int(input("Enter the number\n"))
