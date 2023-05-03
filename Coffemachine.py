37a721eb0171200c8d2632ba63323490ff4f6346
dip980 <khanaldipendra381@gmail.com>
2023-05-03 17:39:24 +0545

    comment added

--- Coffemachine.py
+++ Coffemachine.py
@@ -1,33 +1,33 @@
-# #coffee machine
-# print("Welcome to coffee machine\n")
-# balance=int(input("How much money do you have\n"))
-# while balance>=60:
-#     name=input("Enter the coffee name\n")
-#     if name == "cappucino":
-#         price = 150
-#     elif name == 'latte':
-#         price = 60
-#     elif name == 'americano':
-#         price = 130
-#     else:
-#         print("Please enter correct name.")
+#coffee machine
+print("Welcome to coffee machine\n")
+balance=int(input("How much money do you have\n"))
+while balance>=60:
+    name=input("Enter the coffee name\n")
+    if name == "cappucino":
+        price = 150
+    elif name == 'latte':
+        price = 60
+    elif name == 'americano':
+        price = 130
+    else:
+        print("Please enter correct name.")
         
-#     a=int(input("How much coffee do you want?\n"))
-#     if balance>=price*a:
-#         balance = balance - price*a
-#         print("There ois your coffee. Enjoy .🍵")
-#         print(f"Your remaining balance is: {balance}")
-#     else:
-#         print("Not enough money.")
+    a=int(input("How much coffee do you want?\n"))
+    if balance>=price*a:
+        balance = balance - price*a
+        print("There ois your coffee. Enjoy .🍵")
+        print(f"Your remaining balance is: {balance}")
+    else:
+        print("Not enough money.")
 
-class coffee_machine:
-    def __init__(self,name,price):
-        self.name=name
-        self.price=price
-    def say(self):
-        print(f"My name is {self.name}, ")
-a=input("Enter your name\n")
-print(coffee_machine.self)
+# class coffee_machine:
+#     def __init__(self,name,price):
+#         self.name=name
+#         self.price=price
+#     def say(self):
+#         print(f"My name is {self.name}, ")
+# a=input("Enter your name\n")
+# print(coffee_machine.self)
 
 
     
--- wordguessing.py
+++
@@ -1,7 +0,0 @@
-a="a"
-user_input=input("Guess the number\n").lower()
-if user_input==a:
-    print("You guess the right character\n")
-else:
-    print("Incorrect ! please guess the right character")
-
